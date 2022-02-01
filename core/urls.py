a43ff2dbc8951df86c8953218a7b4b64353ba329
Michael Orji <princemikeorji@gmail.com>
2022-02-01 20:33:57 +0100

    created core view, templates, urls & form

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('create_client/', views.create_client, name="create-client"),
+    path('update_client/<int:pk>/', views.update_client, name="update-client"),
+    path('delete_client/<int:pk>/', views.delete_client, name="delete-client"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,42 @@
+from django.urls import reverse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from .models import Client
+from .forms import ClientForm
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome!")
+    return render(request, 'index.html')
+
+def create_client(request):
+    form = ClientForm
+
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('core:index'))
+    return render(request, 'core/create_client.html', {'form': form})
+
+def update_client(request, pk):
+    client = Client.objects.get(pk=pk)
+    form = ClientForm(instance=client)
+
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('core:index'))
+    return render(request, 'core/update_client.html', {'form': form})
+
+def delete_client(request, pk):
+    client = Client.objects.get(pk=pk)
+
+    if request.method == 'POST':
+            client.delete()
+            return HttpResponseRedirect(reverse('core:index'))
+    return render(request, 'core/delete_client.html', {'obj': client})
+
+
