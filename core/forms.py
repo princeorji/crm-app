6276dc7731d8d22df422e813a111176b8e9080e1
Michael Orji <princemikeorji@gmail.com>
2022-02-02 10:13:49 +0100

    form update

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,10 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from .models import Client
 
 class ClientForm(ModelForm):
     class Meta:
         model = Client
         fields = '__all__'
+        widgets = {'phone_number': TextInput(attrs={
+            'placeholder': '+234'
+            })}
